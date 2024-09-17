fcb057d585943d7a9a3626dda171d044d5252dcb
dannnluvs <110661011+dannnluvs@users.noreply.github.com>
2024-09-17 19:42:40 +0800

    Debug Code

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 int main() {
-    
-    cout << "test";
+    //Debug code
+    int fnum;
+
+    fnum=10
+    snum=20
+    tnum=30
+
+    sum=fnum+snum+tnum;
+
+    cout<<"The sum of"<fnum<<""<<snum<<""<<tnum<"is: "<sum;
 
     return 0;
 }
